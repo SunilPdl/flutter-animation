772bbda76dd4c1d756ebf21aa1fc4ccf8a0ecd68
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-10 17:21:44 +0545

    add : product list

---
+++ first_animation/lib/models/product.dart
@@ -0,0 +1,20 @@
+class Product {
+  final String? title, image;
+
+  Product({this.title, this.image});
+}
+
+List<Product> products = [
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes1.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes2.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoe1.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes1.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes2.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoe1.jpg'),
+  Product(title: 'casual shoes', image: 'assets/shoes/shoes.jpg'),
+  // Product(title: 'casual shoes', image: 'assets/shoes/shoe2.jpg'),
+  // Product(title: 'casual shoes', image: 'assets/shoes/shoe3.jpg'),
+  // Product(title: 'casual shoes', image: 'assets/shoes/shoe4.jpg'),
+];
