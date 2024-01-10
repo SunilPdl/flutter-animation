bf20ebf05a28cd8dfdf8754587b698ee73cd54b7
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-10 17:20:19 +0545

    add : constant file

---
+++ first_animation/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const defaultPadding = 10.0;
+const defaultRadius = 5.0;
+const cartBarHeight = 100.0;
+const defaultHeight = 20.0;
+const headerHeight = 85.0;
+const bgColor = Color(0xFFF6F5F2);
+const primaryColor = Color(0xFF40A944);
