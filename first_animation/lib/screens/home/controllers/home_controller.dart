73d744f87ed73e93335b4e5d8aca62812733e3f7
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-12 23:50:27 +0545

    add : addProductToCart and totalCartItems

--- first_animation/lib/screens/home/controllers/home_controller.dart
+++ first_animation/lib/screens/home/controllers/home_controller.dart
@@ -1,11 +1,29 @@
+import 'package:first_animation/models/product.dart';
+import 'package:first_animation/models/productItem.dart';
 import 'package:flutter/widgets.dart';
 
 enum HomeState { normal, cart }
 
 class HomeController extends ChangeNotifier {
   HomeState homeState = HomeState.normal;
+  List<ProductItem> cart = [];
   void changeHomeState(HomeState state) {
     homeState = state;
     notifyListeners();
   }
+
+  void addProductToCart(Product product) {
+    for (ProductItem item in cart) {
+      if (item.product!.title == product.title) {
+        item.increment();
+        notifyListeners();
+        return;
+      }
+    }
+    cart.add(ProductItem(product: product));
+    notifyListeners();
+  }
+
+  int totalCartItems() => cart.fold(
+      0, (previousValue, element) => previousValue + element.quantity);
 }
