2880a6fee23e064799e97aa691cab1bf7f553c63
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-13 00:36:30 +0545

    add : short cart view

---
+++ first_animation/lib/screens/home/components/cart_short_view.dart
@@ -0,0 +1,53 @@
+import 'package:first_animation/constants.dart';
+import 'package:first_animation/screens/home/controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+
+class CardShortView extends StatelessWidget {
+  const CardShortView({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final HomeController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "Cart",
+          style: TextStyle(fontSize: 18),
+        ),
+        const SizedBox(width: defaultHeight),
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List.generate(
+                controller.cart.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(right: defaultPadding / 2),
+                  child: Hero(
+                    tag: controller.cart[index].product!.title! + "_cartTag",
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage:
+                          AssetImage(controller.cart[index].product!.image!),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Text(
+            controller.totalCartItems().toString(),
+            style: TextStyle(fontWeight: FontWeight.bold, color: primaryColor),
+          ),
+        )
+      ],
+    );
+  }
+}
