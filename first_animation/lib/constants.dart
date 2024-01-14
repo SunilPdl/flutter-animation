0c9693342d2d1861f8e9ff06202cfcc036090a20
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-14 13:20:10 +0545

    add : duration time

--- first_animation/lib/constants.dart
+++ first_animation/lib/constants.dart
@@ -8,3 +8,4 @@ const headerHeight = 85.0;
 const bgColor = Color(0xFFF6F5F2);
 const primaryColor = Color(0xFF40A944);
 const panelTransition = Duration(milliseconds: 350);
+const cardlTransition = Duration(milliseconds: 500);
