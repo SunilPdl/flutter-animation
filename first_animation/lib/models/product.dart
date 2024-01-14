9feaa3fb6ca9db892e27be4fe1c641d3319a00c2
SunilPdl <mrsunilpaudel@gmail.com>
2024-01-14 13:21:42 +0545

    remove : images

--- first_animation/lib/models/product.dart
+++ first_animation/lib/models/product.dart
@@ -14,7 +14,4 @@ List<Product> products = [
   Product(title: 'Casual Shoes', image: 'assets/shoes/shoes2.jpg'),
   Product(title: 'Party Shoes', image: 'assets/shoes/shoe1.jpg'),
   Product(title: 'Normal Shoes', image: 'assets/shoes/shoes.jpg'),
-  // Product(title: 'casual shoes', image: 'assets/shoes/shoe2.jpg'),
-  // Product(title: 'casual shoes', image: 'assets/shoes/shoe3.jpg'),
-  // Product(title: 'casual shoes', image: 'assets/shoes/shoe4.jpg'),
 ];
